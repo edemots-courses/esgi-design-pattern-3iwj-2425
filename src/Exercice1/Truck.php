89975caf28b9f553e378690d741d736a29787a6f
Lucas Ratiaray <dev@lucasratiaray.fr>
2025-01-07 12:01:02 +0100

    exercice 1 done

--- src/Exercice1/Truck.php
+++ src/Exercice1/Truck.php
@@ -2,20 +2,20 @@
 
 namespace EdemotsCourses\EsgiDesignPattern\Exercice1;
 
-class Car implements Vehicle
+class Truck implements Vehicle
 {
     public float $speed = 0;
 
     public function accelerate(): float
     {
-        $this->speed += 3.5;
+        $this->speed += 1.75;
         return $this->speed;
     }
 
     public function breaks(): float
     {
-        $this->speed -= 5;
-        
+        $this->speed -= 2;
+
         if ($this->speed < 0) {
             return $this->speed = 0;
         }
