b09dce15943f1ce24248fbc73d43a750a02cf43b
Jonathan NG <real.darksun29@gmail.com>
2025-01-07 15:00:47 +0100

    Add pizza customization methods to CanBuildPizza interface and implement in Pizza class

--- src/Exercice3/CanBuildPizza.php
+++ src/Exercice3/CanBuildPizza.php
@@ -5,6 +5,11 @@ namespace EdemotsCourses\EsgiDesignPattern\Exercice3;
 interface CanBuildPizza
 {
     public function reset(): self;
-    /* setters, adders */
+    public function setSize(string $size): self;
+    public function setCrust(string $crust): self;
+    public function setSauce(string $sauce): self;
+    public function setCheese(string $cheese): self;
+    public function addTopping(string $topping): self;
+    public function setCookingInstructions(string $instructions): self;
     public function build(): Pizza;
 }
--- src/Exercice3/Pizza.php
+++ src/Exercice3/Pizza.php
@@ -4,4 +4,48 @@ namespace EdemotsCourses\EsgiDesignPattern\Exercice3;
 
 class Pizza
 {
+    private string $size;
+    private string $crust;
+    private string $sauce;
+    private string $cheese;
+    private array $toppings = [];
+    private string $cookingInstructions;
+
+    public function setSize(string $size): void
+    {
+        $this->size = $size;
+    }
+
+    public function setCrust(string $crust): void
+    {
+        $this->crust = $crust;
+    }
+
+    public function setSauce(string $sauce): void
+    {
+        $this->sauce = $sauce;
+    }
+
+    public function setCheese(string $cheese): void
+    {
+        $this->cheese = $cheese;
+    }
+
+    public function addTopping(string $topping): void
+    {
+        if (count($this->toppings) < 8) {
+            $this->toppings[] = $topping;
+        }
+    }
+
+    public function setCookingInstructions(string $instructions): void
+    {
+        $this->cookingInstructions = $instructions;
+    }
+
+    public function __toString(): string
+    {
+        return "Pizza: $this->size, $this->crust crust, $this->sauce sauce, $this->cheese cheese, Toppings: " . implode(', ', $this->toppings) . ", Cooking Instructions: $this->cookingInstructions";
+    }
 }
+
