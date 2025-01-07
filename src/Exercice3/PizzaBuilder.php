4078d2c8dfd5247c2495869aab91c53ef00b9f0e
Bilal2504 <bilaldrame2504@gmail.com>
2025-01-07 14:56:52 +0100

    Exo 1 à 3

--- src/Exercice3/PizzaBuilder.php
+++ src/Exercice3/PizzaBuilder.php
@@ -4,4 +4,59 @@ namespace EdemotsCourses\EsgiDesignPattern\Exercice3;
 
 class PizzaBuilder implements CanBuildPizza
 {
+    private Pizza $pizza;
+
+    public function __construct()
+    {
+        $this->reset();
+    }
+
+    public function reset(): self
+    {
+        $this->pizza = new Pizza();
+        return $this;
+    }
+
+    public function setSize(string $size): self
+    {
+        $this->pizza->setSize($size);
+        return $this;
+    }
+
+    public function setCrustType(string $crustType): self
+    {
+        $this->pizza->setCrustType($crustType);
+        return $this;
+    }
+
+    public function setSauce(string $sauce): self
+    {
+        $this->pizza->setSauce($sauce);
+        return $this;
+    }
+
+    public function addCheese(string $cheese): self
+    {
+        $this->pizza->addCheese($cheese);
+        return $this;
+    }
+
+    public function addTopping(string $topping): self
+    {
+        $this->pizza->addTopping($topping);
+        return $this;
+    }
+
+    public function setCookingInstructions(string $instructions): self
+    {
+        $this->pizza->setCookingInstructions($instructions);
+        return $this;
+    }
+
+    public function build(): Pizza
+    {
+        $builtPizza = $this->pizza;
+        $this->reset();
+        return $builtPizza;
+    }
 }
