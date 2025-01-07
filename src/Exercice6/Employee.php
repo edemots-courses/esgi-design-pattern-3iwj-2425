0cd538f9daf3d29ca8d1713ec2ce933dc3edcb62
“Mélina <mterrier4@myges.fr>
2025-01-07 18:54:33 +0100

    exercice 6

--- src/Exercice6/Employee.php
+++ src/Exercice6/Employee.php
@@ -4,6 +4,17 @@ namespace EdemotsCourses\EsgiDesignPattern\Exercice6;
 
 class Employee implements OrganizationUnit
 {
+    private int $id;
+    private string $name;
+    private string $jobTitle;
+
+    public function __construct(int $id, string $name, string $jobTitle)
+    {
+        $this->id = $id; // Initialisation de $id
+        $this->name = $name;
+        $this->jobTitle = $jobTitle;
+    }
+
     public function getId(): int
     {
         return $this->id;
@@ -18,4 +29,12 @@ class Employee implements OrganizationUnit
     {
         return $this->jobTitle;
     }
+
+    public function displayDetails(int $indentation = 0): string
+    {
+        return str_repeat("    ", $indentation) . "Employee ID : " . $this->id . "\n" .
+               str_repeat("    ", $indentation) . "Employee name : " . $this->name . "\n" . 
+               str_repeat("    ", $indentation) . "Employee job title : " . $this->jobTitle;
+    }    
+
 }
--- src/Exercice6/Organization.php
+++ src/Exercice6/Organization.php
@@ -1,11 +1,48 @@
 <?php
-
 namespace EdemotsCourses\EsgiDesignPattern\Exercice6;
 
 class Organization implements OrganizationUnit
 {
+    private string $name;
+    private array $organizationUnits = [];
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
     public function getName(): string
     {
         return $this->name;
+    }
+
+    public function addOrganizationUnit(OrganizationUnit $unit)
+    {
+        $this->organizationUnits[] = $unit;
+    }
+
+    public function removeOrganizationUnit(OrganizationUnit $unit)
+    {
+        $key = array_search($unit, $this->organizationUnits);
+        if ($key !== false) {
+            unset($this->organizationUnits[$key]);
+        }
+    }
+
+    public function displayDetails(int $indentation = 0): string
+    {
+        $details = "Organization name : " . $this->name . "\n" . "Organization details :\n";
+        $organizationUnits = $this->organizationUnits ?? [];
+
+        $lastUnitIndex = count($organizationUnits) - 1;
+
+        foreach ($organizationUnits as $index => $unit) {
+            $details .= $unit->displayDetails($indentation + 1);
+            if ($index !== $lastUnitIndex) {
+                $details .= "\n";
+            }
+        }
+
+        return $details;
     }
 }
