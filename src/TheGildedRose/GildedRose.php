653ff5ea9fa5f2b1abbe6cabc1b42061023c8315
“Mélina <mterrier4@myges.fr>
2025-01-09 18:04:50 +0100

    Tp

--- src/TheGildedRose/GildedRose.php
+++ src/TheGildedRose/GildedRose.php
@@ -22,51 +22,103 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+            $strategy = $this->getUpdateStrategy($item);
+            $strategy->updateQuality($item);  
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
+            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
+                $item->sellIn -= 1;
             }
 
             if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
+                $strategy->updateQuality($item);
             }
         }
+    }
+
+
+    private function getUpdateStrategy(Item $item): QualityUpdateStrategy
+    {
+        switch ($item->name) {
+            case 'Aged Brie':
+                return new AgedBrieQualityUpdateStrategy();
+            case 'Backstage passes to a TAFKAL80ETC concert':
+                return new BackstagePassesQualityUpdateStrategy();
+            case 'Sulfuras, Hand of Ragnaros':
+                return new SulfurasQualityUpdateStrategy();
+            case strpos($item->name, 'Conjured') !== false:
+                return new ConjuredItemQualityUpdateStrategy();
+            default:
+                return new NormalItemQualityUpdateStrategy();
+        }
+    }
+}
+
+interface QualityUpdateStrategy
+{
+    public function updateQuality(Item $item): void;
+}
+
+class NormalItemQualityUpdateStrategy implements QualityUpdateStrategy
+{
+    public function updateQuality(Item $item): void
+    {
+        if ($item->quality > 0) {
+            $item->quality -= 1;
+        }
+    }
+}
+
+class AgedBrieQualityUpdateStrategy implements QualityUpdateStrategy
+{
+    public function updateQuality(Item $item): void
+    {
+        if ($item->quality < 50) {
+            $item->quality += 1;
+        }
+    }
+}
+
+class BackstagePassesQualityUpdateStrategy implements QualityUpdateStrategy
+{
+    public function updateQuality(Item $item): void
+    {
+        if ($item->quality < 50) {
+            $item->quality += 1;
+
+            if ($item->sellIn < 11) {
+                $item->quality += 1;
+            }
+            if ($item->sellIn < 6) {
+                $item->quality += 1;
+            }
+        }
+
+        // Assurer que la qualité ne dépasse jamais 50
+        if ($item->quality > 50) {
+            $item->quality = 50;
+        }
+
+        // Après la date de péremption, la qualité devient 0
+        if ($item->sellIn < 0) {
+            $item->quality = 0;
+        }
+    }
+}
+
+
+class ConjuredItemQualityUpdateStrategy implements QualityUpdateStrategy
+{
+    public function updateQuality(Item $item): void
+    {
+        if ($item->quality > 0) {
+            $item->quality -= 2;
+        }
+    }
+}
+
+class SulfurasQualityUpdateStrategy implements QualityUpdateStrategy
+{
+    public function updateQuality(Item $item): void
+    {
     }
 }
